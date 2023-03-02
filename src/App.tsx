c96a5f259d0a83ccc743cbc25d7e1bbec3ee9d5c
AnaCatanaR <anacatana.ac@gmail.com>
2023-03-02 22:08:04 +0100

    Add GlobalStyles to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import "./GlobalStyles.css";
+import Key from "./components/Key/Key";
+
 const App = (): JSX.Element => {
-  return <div></div>;
+  return <Key className={"addition"} content={"2"} />;
 };
 
 export default App;
